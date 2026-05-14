cf91e1189fa328c65c7fb5fefe90e8249c440c08
JesusLuis493 <jemanuelluissandoval@gmail.com>
2026-05-14 11:09:05 -0600

    create: repositorio nuevo

---
+++ src/negocio_mi/lupita/Negocio_miLupita.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package negocio_mi.lupita;
+
+/**
+ *
+ * @author jesus-luis
+ */
+public class Negocio_miLupita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
